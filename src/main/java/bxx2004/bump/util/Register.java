7178bfe50488613e1e538b4aa61d22a3226bf9de
haiman <83174104+haiman233@users.noreply.github.com>
2021-09-08 09:15:34 +0800

    Update Register.java

--- src/main/java/bxx2004/bump/util/Register.java
+++ src/main/java/bxx2004/bump/util/Register.java
@@ -1,6 +1,6 @@
-/*    */ package bxx2004.bump.util;
-/*    */ 
-/*    */
+ package bxx2004.bump.util;
+ 
+
 
 import bxx2004.bump.Bump;
 import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
@@ -10,34 +10,22 @@ import org.bukkit.plugin.java.JavaPlugin;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-/*    */
-/*    */
-/*    */
-/*    */
-/*    */
 
-/*    */
-/*    */ public class Register
-/*    */   implements SlimefunAddon
-/*    */ {
-/*    */   @Nonnull
-/*    */   public JavaPlugin getJavaPlugin() {
-/* 15 */     return Bump.getPlugin(Bump.class);
-/*    */   }
-/*    */   
-/*    */   @Nullable
-/*    */   public String getBugTrackerURL() {
-/* 20 */     return "bxx2004";
-/*    */   }
-/*    */   
-/*    */   public Register(SlimefunItem... slimefunItems) {
-/* 24 */     for (int i = 0; i < slimefunItems.length; i++)
-/* 25 */       slimefunItems[i].register(this); 
-/*    */   }
-/*    */ }
-
-
-/* Location:              C:\Users\Administrator\Desktop\bump测试\【魔法】Bump_v1.0.jar!\bxx2004\bum\\util\Register.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
+ public class Register
+   implements SlimefunAddon
+ {
+   @Nonnull
+   public JavaPlugin getJavaPlugin() {
+     return Bump.getPlugin(Bump.class);
+   }
+   
+   @Nullable
+   public String getBugTrackerURL() {
+     return "bxx2004";
+   }
+   
+   public Register(SlimefunItem... slimefunItems) {
+     for (int i = 0; i < slimefunItems.length; i++)
+       slimefunItems[i].register(this); 
+   }
+ }
