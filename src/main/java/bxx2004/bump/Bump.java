84c15ada312dce7d76d5a258fc01f0b1a9635797
ybw0014 <i@guizhanss.com>
2022-05-18 15:46:04 -0400

    Add chart to metrics

--- src/main/java/bxx2004/bump/Bump.java
+++ src/main/java/bxx2004/bump/Bump.java
@@ -2,6 +2,7 @@ package bxx2004.bump;
 
 import bxx2004.bump.listeners.BowShootListener;
 import bxx2004.bump.setup.ItemsSetup;
+import net.guizhanss.guizhanlib.bstats.charts.SimplePie;
 import net.guizhanss.guizhanlib.slimefun.addon.AbstractAddon;
 
 public final class Bump extends AbstractAddon {
@@ -24,8 +25,10 @@ public final class Bump extends AbstractAddon {
         sendConsole("&6&l     \\/___/  \\/___/  \\/_/\\/_/\\/_/\\ \\ \\/ ");
         sendConsole("&6&l                                  \\ \\_\\ ");
         sendConsole("&6&l                                   \\/_/ ");
-        sendConsole("&a&l     - Slimefun4 RC-30+ - Powered By bxx2004");
-        sendConsole("&a&l     - Website https://github.com/SlimefunGuguProject/Bump");
+        sendConsole("&a&l  Bump 2 for Slimefun4 RC-30+");
+        sendConsole("&a&l  Powered By bxx2004");
+        sendConsole("&a&l  Repository: https://github.com/SlimefunGuguProject/Bump");
+        sendConsole("&a&l  Issue tracker: https://github.com/SlimefunGuguProject/Bump/issues");
 
         // localization
         String lang = getConfig().getString("lang", "zh-CN");
@@ -33,9 +36,12 @@ public final class Bump extends AbstractAddon {
         localization.addLanguage(lang);
         sendConsole("&eLoaded language {0}", lang);
 
+        // items setup
         ItemsSetup.setup();
 
         getServer().getPluginManager().registerEvents(new BowShootListener(), this);
+
+        getMetrics().addCustomChart(new SimplePie("language", () -> lang));
     }
 
     @Override
