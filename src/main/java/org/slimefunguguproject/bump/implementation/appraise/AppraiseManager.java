25b57179b0076d03c096210c13300bbdfa404799
haiman <83174104+haiman233@users.noreply.github.com>
2022-06-03 10:40:09 +0800

    Update AppraiseManager.java

--- src/main/java/org/slimefunguguproject/bump/implementation/appraise/AppraiseManager.java
+++ src/main/java/org/slimefunguguproject/bump/implementation/appraise/AppraiseManager.java
@@ -29,18 +29,31 @@ public final class AppraiseManager {
 
     private void setup() {
         weaponAttrs = new AppraiseAttributes()
-            .add(Attribute.GENERIC_ATTACK_DAMAGE, 0, 30)
-            .add(Attribute.GENERIC_ATTACK_SPEED, -3, 10)
-            .add(Attribute.GENERIC_MOVEMENT_SPEED, -0.4, 0.6)
-            .add(Attribute.GENERIC_LUCK, -3, 10)
-            .add(Attribute.GENERIC_ATTACK_KNOCKBACK, -2, 12)
+            .add(Attribute.GENERIC_ATTACK_DAMAGE, 0, 30, 40)
+            .add(Attribute.GENERIC_ATTACK_SPEED, -3, 10, 25)
+            .add(Attribute.GENERIC_MOVEMENT_SPEED, -0.4, 0.6, 15)
+            .add(Attribute.GENERIC_LUCK, -3, 10, 10)
+            .add(Attribute.GENERIC_ATTACK_KNOCKBACK, -2, 12, 10)
             .build();
 
         // TODO: armor & horse armor
         armorAttrs = new AppraiseAttributes()
+            .add(Attribute.GENERIC_ARMOR, 0, 30, 40)
+            .add(Attribute.GENERIC_ARMOR_TOUGHNESS, -2, 12, 25)
+            .add(Attribute.GENERIC_MAX_HEALTH, -5, 12, 15)
+            .add(Attribute.GENERIC_KNOCKBACK_RESISTANCE, -0.2, 0.8, 10)
+            .add(Attribute.GENERIC_FLYING_SPEED, -3, 5, 7)
+            .add(Attribute.ZOMBIE_SPAWN_REINFORCEMENTS, -5, 5, 3)
             .build();
 
         horseArmorAttrs = new AppraiseAttributes()
+            .add(Attribute.GENERIC_MAX_HEALTH, 0, 30, 30)
+            .add(Attribute.GENERIC_ARMOR, -5, 30, 15)
+            .add(Attribute.GENERIC_ARMOR_TOUGHNESS, -2, 12, 10)
+            .add(Attribute.GENERIC_KNOCKBACK_RESISTANCE, -0.2, 0.8, 5)
+            .add(Attribute.HORSE_JUMP_STRENGTH, -0.5, 1.4, 20)
+            .add(Attribute.GENERIC_MOVEMENT_SPEED, -0.5, 1.2, 15)
+            .add(Attribute.GENERIC_FOLLOW_RANGE, -50, 250, 5)
             .build();
     }
 
