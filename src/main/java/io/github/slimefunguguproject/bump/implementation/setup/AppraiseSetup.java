cbe5c80cfa80c37b1f7047fab4fd29316fc6f350
ybw0014 <i@guizhanss.com>
2022-07-17 01:16:09 -0400

    Javadocs

--- src/main/java/io/github/slimefunguguproject/bump/implementation/setup/AppraiseSetup.java
+++ src/main/java/io/github/slimefunguguproject/bump/implementation/setup/AppraiseSetup.java
@@ -1,4 +1,15 @@
 package io.github.slimefunguguproject.bump.implementation.setup;
 
+import lombok.experimental.UtilityClass;
+
+/**
+ * This class set up the default appraisal types.
+ *
+ * @author ybw0014
+ */
+@UtilityClass
 public final class AppraiseSetup {
+    public static void setup() {
+
+    }
 }
